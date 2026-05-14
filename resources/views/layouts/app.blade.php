f41c798ba93e318b46f7e18f028b5f8233df150d
wehgus <nugrohogusti1999@gmail.com>
2026-05-14 10:41:44 +0700

    fix : tailwind css lokal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,9 @@
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Tailwind CSS via CDN -->
-    <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    
     <style>
         /* Custom Styles */
         ::-webkit-scrollbar {
